c690da98da31aa1550dde074bdd79003917e88f7
Mohamad Mustapha <mwm19@mail.aub.edu>
2023-12-14 21:24:24 +0200

    project init

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by mohamad mostapha on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
